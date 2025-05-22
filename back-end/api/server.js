503eaaaf9aa76724c18ea82804e0de05efea06a2
MuriloLucena08 <muriloclucena@gmail.com>
2025-05-22 15:25:49 -0300

    Ajuste no process novamente

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,41 +1,37 @@
 // API Application Programing Interface
 import express from "express";
 import cors from "cors";
-import {db} from "./connect.js";
-import path, { dirname } from "path";
+import { db } from "./connect.js";
+import path from "path"
 
-const _dirname =path.resolve();
+const __dirname = path.resolve();
+console.log(__dirname);
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-let artists = await db.
-  collection("artists").
-  find({}).
-  toArray();
-let songs = await db.
-  collection("songs").
-  find({}).
-  toArray();
+let artists = await db.collection("artists").find({}).toArray();
+let songs = await db.collection("songs").find({}).toArray();
 
-app.get("/api/", (req,res) => {
+app.get("/api/", (req, res) => {
   res.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/api/artists", async (req,res) => {
+app.get("/api/artists", async (req, res) => {
   res.send(artists);
 });
 
-app.get("/api/songs", async (req,res) => {
+app.get("/api/songs", async (req, res) => {
   res.send(songs);
 });
 
-app.use(express.static(path.join(_dirname,'../front-end/dist')));
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
 
-app.get("/*",  async (req,res) => {
-  res.sendFile(path.join(_dirname,'../front-end/dist/index.html'));
+app.get("/*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,9 +2,10 @@
 import axios from "axios";
 
 // const {NODE_ENV} = process.env;
-// const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const URL = localhost:3000/api";
 
 const URL = "https://deploy-full-stack-tyj4.onrender.com/api";
+
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
